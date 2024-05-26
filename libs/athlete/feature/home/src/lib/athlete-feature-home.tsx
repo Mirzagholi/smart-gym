5c8ece9a104c0f1e2d75a7eea092147e5dabd3bc
mirzagholi <m.mirzagholi@megagroup.biz>
2024-05-26 17:33:48 +0330

    feat: extend color in tailwind

--- libs/athlete/feature/home/src/lib/athlete-feature-home.tsx
+++ libs/athlete/feature/home/src/lib/athlete-feature-home.tsx
@@ -4,7 +4,7 @@ export interface AthleteFeatureHomeProps {}
 export function AthleteFeatureHome(props: AthleteFeatureHomeProps) {
   return (
     <div>
-      <h1 className="bg-red-500">به آپ اسمارت جیم خوش آمدید</h1>
+      <h1 className="bg-primary-200">به آپ اسمارت جیم خوش آمدید</h1>
     </div>
   );
 }
