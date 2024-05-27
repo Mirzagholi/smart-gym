4726c0331261a3bd08507eff68621b111f705277
nedaniku <neda.nikooy78@gmail.com>
2024-05-27 12:18:07 +0330

    feature/issue-102/create messages  page

---
+++ apps/athlete/src/app/received-messages/page.tsx
@@ -0,0 +1,93 @@
+import Image from "next/image"
+/* eslint-disable-next-line */
+export interface MessagesProps {}
+
+export default function Messages(props: MessagesProps) {
+  return (
+    <>
+     <div className="bg-primary-600 ">
+        
+         <section className=" container relative z-10  bg-white bg- rounded-t-4xl">
+            
+           
+
+           <div className="mx-8 pt-8 space-y-4">
+               <div className="border border-primary-100 rounded-2xl px-4 py-2">
+                <div className="flex justify-between items-center p-2">
+                    <p className="text-base text-primary-600">ساعات کار باشگاه</p>
+                    <div><Image width={18} height={18} src="/media/home/images/arrow1.svg" alt="" /></div>
+                </div>
+                <hr className="py-1"/>
+                <p className="text-sm text-primary-600 px-2 leading-7">با تبریک آغاز سال نو، ساعات کار باشگاه از روز 15 فروردسن به روال قبل از تعطیلات نوروز خواهد بود.</p>
+                <div className="flex gap-2 justify-end px-2">
+                    <div><Image width={16} height={16} src="/media/home/images/Vector(6).svg" alt="" /></div>
+                    <span className="text-primary-500 text-sm">1403/02/28</span>
+                </div>
+               </div>
+
+
+               <div className="border border-primary-100 rounded-2xl px-4 py-2">
+                <div className="flex justify-between items-center p-2">
+                    <p className="text-base text-primary-600">تبریک سال نو و نوروز 1403</p>
+                    <div><Image width={18} height={18} src="/media/home/images/arrow1.svg" alt="" /></div>
+                </div>
+                <hr className="py-1"/>
+                <p className="text-sm text-primary-600 px-2 leading-7">با تبریک آغاز سال نو، ساعات کار باشگاه از روز 15 فروردسن به روال قبل از تعطیلات نوروز خواهد بود.</p>
+                <div className="flex gap-2 justify-end px-2">
+                    <div><Image width={16} height={16} src="/media/home/images/Vector(6).svg" alt="" /></div>
+                    <span className="text-primary-500 text-sm">1403/02/28</span>
+                </div>
+               </div>
+
+
+
+               <div className="border border-primary-100 rounded-2xl px-4 py-2">
+                <div className="flex justify-between items-center p-2">
+                    <p className="text-base text-primary-600">ساعات کار باشگاه در ایام تعطیل</p>
+                    <div><Image width={18} height={18} src="/media/home/images/arrow1.svg" alt="" /></div>
+                </div>
+                <hr className="py-1"/>
+                <p className="text-sm text-primary-600 px-2 leading-7">با تبریک آغاز سال نو، ساعات کار باشگاه از روز 15 فروردسن به روال قبل از تعطیلات نوروز خواهد بود.</p>
+                <div className="flex gap-2 justify-end px-2">
+                    <div><Image width={16} height={16} src="/media/home/images/Vector(6).svg" alt="" /></div>
+                    <span className="text-primary-500 text-sm">1403/02/28</span>
+                </div>
+               </div>
+
+
+               <div className="border border-primary-100 rounded-2xl px-4 py-2">
+                <div className="flex justify-between items-center p-2">
+                    <p className="text-base text-primary-600"> تبریک سال نو و نوروز 1403</p>
+                    <div><Image width={18} height={18} src="/media/home/images/arrow1.svg" alt="" /></div>
+                </div>
+                <hr className="py-1"/>
+                <p className="text-sm text-primary-600 px-2 leading-7">با تبریک آغاز سال نو، ساعات کار باشگاه از روز 15 فروردسن به روال قبل از تعطیلات نوروز خواهد بود.</p>
+                <div className="flex gap-2 justify-end px-2">
+                    <div><Image width={16} height={16} src="/media/home/images/Vector(6).svg" alt="" /></div>
+                    <span className="text-primary-500 text-sm">1403/02/28</span>
+                </div>
+               </div>
+
+
+               <div className="border border-primary-100 rounded-2xl px-4 py-2">
+                <div className="flex justify-between items-center p-2">
+                    <p className="text-base text-primary-600">ساعات کار باشگاه در ایام تعطیل</p>
+                    <div><Image width={18} height={18} src="/media/home/images/arrow1.svg" alt="" /></div>
+                </div>
+                <hr className="py-1"/>
+                <p className="text-sm text-primary-600 px-2 leading-7">با تبریک آغاز سال نو، ساعات کار باشگاه از روز 15 فروردسن به روال قبل از تعطیلات نوروز خواهد بود.</p>
+                <div className="flex gap-2 justify-end px-2">
+                    <div><Image width={16} height={16} src="/media/home/images/Vector(6).svg" alt="" /></div>
+                    <span className="text-primary-500 text-sm">1403/02/28</span>
+                </div>
+               </div>
+            
+           </div>
+
+
+            
+        </section>
+   </div>
+    </>
+  );
+}
